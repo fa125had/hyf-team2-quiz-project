648c03e58d31dd1b1a6358bc78fa110d19d006fa
farshad <far5had@outlook.com>
2023-06-20 19:11:58 +0200

    distinguish between correct and incorrect answers

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -2,21 +2,32 @@
  * Create an Answer element
  * @returns {Element}
  */
+import { quizData } from '../data.js';
+
 export const createAnswerElement = (key, answerText) => {
   const element = document.createElement('li');
   let isSelected = false;
 
   const handleClick = ({ target }) => {
     const selectedElement = document.querySelector('.selected');
+    const correctAnswer =
+      quizData.questions[quizData.currentQuestionIndex].correct;
+
     if (selectedElement) {
       selectedElement.classList.remove('selected');
       selectedElement.style.backgroundColor = 'transparent';
     }
-    target.style.backgroundColor = '#fff';
+    if (key === correctAnswer) {
+      target.style.backgroundColor = 'green';
+    } else {
+      target.style.backgroundColor = 'red';
+    }
     target.classList.add('selected');
   };
 
-  element.addEventListener('click', handleClick);
+  element.addEventListener('click', (event) => {
+    handleClick(event);
+  });
 
   element.innerHTML = String.raw`
     ${key}: ${answerText};
