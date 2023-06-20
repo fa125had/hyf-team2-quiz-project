3b9ff8e516a8af0d2b5cde6fe18041c9f948ba4e
farshad <far5had@outlook.com>
2023-06-20 17:42:50 +0200

    early version of show selected answer to user

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -4,6 +4,20 @@
  */
 export const createAnswerElement = (key, answerText) => {
   const element = document.createElement('li');
+  let isSelected = false;
+
+  const handleClick = ({ target }) => {
+    const selectedElement = document.querySelector('.selected');
+    if (selectedElement) {
+      selectedElement.classList.remove('selected');
+      selectedElement.style.backgroundColor = 'transparent';
+    }
+    target.style.backgroundColor = '#fff';
+    target.classList.add('selected');
+  };
+
+  element.addEventListener('click', handleClick);
+
   element.innerHTML = String.raw`
     ${key}: ${answerText};
   `;
