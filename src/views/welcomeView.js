7660a01e4aed00aaaddd2caf3114936f867c8687
Valentyn Genkin <genkin.valentyn@gmail.com>
2023-06-20 13:47:00 +0200

    added ID to welcome element

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -1,12 +1,14 @@
 import { START_QUIZ_BUTTON_ID } from '../constants.js';
 import { WELCOME_PAGE_PARAGRAPH_ID } from '../constants.js';
 import { WELCOME_TITLE_ID } from '../constants.js';
+import { WELCOME_PAGE_ELEMENT } from '../constants.js';
 /**
  * Create the welcome screen
  * @returns {Element}
  */
 export const createWelcomeElement = () => {
   const element = document.createElement('div');
+  element.id = `${WELCOME_PAGE_ELEMENT}`
   element.innerHTML = String.raw`
     <h1 id="${WELCOME_TITLE_ID}">Welcome</h1>
     <p id="${WELCOME_PAGE_PARAGRAPH_ID}">Take this quick 10-question
