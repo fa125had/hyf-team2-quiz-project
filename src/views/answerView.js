31232a8b001ef44f4c4915a406833940e1c8fd92
Seyma <seymabaskan@hotmail.com>
2023-06-21 14:33:10 +0200

    A user can select and see the correct answer-updated

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -6,7 +6,7 @@ export const createAnswerElement = (key, answerText) => {
   const element = document.createElement('li');
   const optionButton = document.createElement('button');
   optionButton.type = 'button';
-  optionButton.id = `${key}`;
+  optionButton.id = key;
   optionButton.innerHTML = String.raw`${key} : ${answerText}`;
   element.appendChild(optionButton);
 
