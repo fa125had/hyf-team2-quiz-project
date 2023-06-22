5a435bfe597df519af9f9d8af68f31ee17949ca0
Valentyn Genkin <genkin.valentyn@gmail.com>
2023-06-22 10:48:51 +0200

    added function to get name input value

--- src/pages/welcomePage.js
+++ src/pages/welcomePage.js
@@ -1,4 +1,8 @@
-import { USER_INTERFACE_ID, START_QUIZ_BUTTON_ID } from '../constants.js';
+import {
+  USER_INTERFACE_ID,
+  START_QUIZ_BUTTON_ID,
+  WELCOME_PAGE_INPUT,
+} from '../constants.js';
 import { createWelcomeElement } from '../views/welcomeView.js';
 import { initQuestionPage } from './questionPage.js';
 import { createWelcomePageCover } from '../views/welcomeView.js';
@@ -15,8 +19,19 @@ export const initWelcomePage = () => {
 
   document
     .getElementById(START_QUIZ_BUTTON_ID)
+    .addEventListener('click', getName);
+
+  document
+    .getElementById(START_QUIZ_BUTTON_ID)
     .addEventListener('click', startQuiz);
 };
+
+const getName = () => {
+  const inputValue = document.getElementById(WELCOME_PAGE_INPUT).value;
+  return inputValue;
+};
+
+// Need to do - stop loading if input is empty
 
 const startQuiz = () => {
   initQuestionPage();
